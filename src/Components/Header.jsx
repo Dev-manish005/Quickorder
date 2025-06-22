8f3ca2644ded80b5328c22bd7f1aaef679884cfd
Manish Pradhan <manish.j.p.005@gmail.com>
2025-06-23 02:02:41 +0530

    Header Component Created.

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+
+const Header = () =>{
+    return(
+        <>
+            <div className="flex justify-between p-5 bg-orange-500">
+                <div>
+                    <img src=""alt="Munch Point"/>
+                </div>
+                <div className="flex gap-7">
+                    <div>Home🏠</div>
+                    <div>About us</div>
+                    <div>Contact us📞</div>
+                    <div>Cart🛒</div>
+                </div>    
+            </div>        
+        </>
+    )
+}
+
+export default Header
