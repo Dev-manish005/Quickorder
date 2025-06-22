1ccd7a3c83dce98c22a98827ba1cb52e34903488
Manish Pradhan <manish.j.p.005@gmail.com>
2025-06-22 14:17:26 +0530

    Initial project setup with Tailwind css.

---
+++ src/Components.jsx
@@ -0,0 +1,13 @@
+const Header=()=>{
+     return (
+        <>
+            <div id="header">
+                <div>
+                    
+                </div>
+            </div>
+        </>
+     )
+}
+
+export default Header
