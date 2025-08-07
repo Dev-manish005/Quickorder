4ff0daa30076792164a65fbdd32f2c8fdfaf332b
Manish Pradhan <manish.j.p.005@gmail.com>
2025-08-07 17:56:16 +0530

    made some changes in header component.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router";
 import logo from "../assets/logo.png";
 import Searchbar from "./Searchbar";
 
@@ -10,8 +10,8 @@ const Header = () => {
         <div className="flex flex-col sm:flex-row items-center justify-between py-4 space-y-4 sm:space-y-0">
           
           {/* Logo */}
-          <div className="flex items-center  space-x-2">
-            <img src={logo} alt="Food Point Logo" className="h-10 sm:h-12" />
+          <div className="flex items-center  space-x-2 ">
+           <Link to ="/"><img src={logo} alt="Food Point Logo" className="h-10 sm:h-12 hover:h-[52px] transition-all duration-300 cursor-pointer rounded" /></Link>
            
           </div>
 
@@ -21,13 +21,11 @@ const Header = () => {
 
           {/* Navigation links */}
           <nav className="flex space-x-4 text-orange-500 text-sm sm:text-base font-semibold">
-            <a href="#" className="hover:text-orange-600">Home</a>
-            <a href="#" className="hover:text-orange-600">About</a>
-           
-            <a href="#" className="hover:text-orange-600">Contact</a>
-            <a href="#" className="hover:text-orange-600">Cart</a>
+            <Link to="/" className="hover:text-orange-600 hover:scale-110 transition-transform duration-200">Home</Link>
+            <Link to="/about" className="hover:text-orange-600 hover:scale-110 transition-transform duration-200">About</Link>
+            <Link to="/contact" className="hover:text-orange-600 hover:scale-110 transition-transform duration-200">Contact</Link>
+            <Link to="/cart" className="hover:text-orange-600 hover:scale-110 transition-transform duration-200">Cart</Link>
           </nav>
-
         </div>
       </div>
     
